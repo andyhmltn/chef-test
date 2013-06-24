916ba344b8fac7926a6d0ab8a4a952f2d433abb4
Andy Hamilton <andyhmltn@gmail.com>
2013-06-24 16:41:11 +0100

    Added a functioning script (kind of)

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Usage: ./deploy.sh [host]
+
+host="${1}"
+
+ssh-keygen -R "${host#*@}" 2> /dev/null
+
+tar cj . | ssh -o 'StrictHostKeyChecking no' "$host" '
+sudo rm -rf ~/chef &&
+mkdir ~/chef &&
+cd ~/chef &&
+tar xj &&
+sudo bash install.sh'
